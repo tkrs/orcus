7d571876cfe50e60929005159253165e40183088
Takeru Sato <type.in.type@gmail.com>
2018-02-19 17:34:54 +0900

    Remove the unnecessary ExecutionContext

--- modules/core/src/main/scala/orcus/async/ScalaFutureAsyncHandlerInstances.scala
+++ modules/core/src/main/scala/orcus/async/ScalaFutureAsyncHandlerInstances.scala
@@ -1,22 +1,20 @@
 package orcus.async
 
-import scala.concurrent.{ExecutionContext, Future, Promise}
+import scala.concurrent.{Future, Promise}
 
 trait ScalaFutureAsyncHandlerInstances {
 
-  implicit def handleScalaFuture(implicit ec: ExecutionContext): AsyncHandler[Future] =
+  implicit lazy val handleScalaFuture: AsyncHandler[Future] =
     new AsyncHandler[Future] {
       def handle[A](callback: Callback[A], cancel: => Unit): Future[A] = {
         val p = Promise[A]
 
-        ec.execute(new Runnable {
-          def run(): Unit = callback {
-            case Left(e) =>
-              val _ = p.failure(e)
-            case Right(v) =>
-              val _ = p.success(v)
-          }
-        })
+        callback {
+          case Left(e) =>
+            val _ = p.failure(e)
+          case Right(v) =>
+            val _ = p.success(v)
+        }
 
         p.future
       }
