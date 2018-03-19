6e0642763cdd653c68db9070808e2401202a4371
Takeru Sato <type.in.type@gmail.com>
2018-03-20 02:38:30 +0900

    Update to hbase client 2.0.0-beta-2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
     val scalatest        = "3.0.5"
     val mockito          = "2.13.0"
     val kindProjector    = "0.9.6"
-    val hbase            = "2.0.0-beta-1"
+    val hbase            = "2.0.0-beta-2"
     val bigtable         = "1.2.0"
     val logback          = "1.2.3"
     val scalaJava8Compat = "0.8.0"
