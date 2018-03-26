1839e6dd987c2222160f63afcf1660c711ed7c79
Takeru Sato <type.in.type@gmail.com>
2018-03-26 22:12:04 +0900

    Update to Scala 2.12.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   val Ver = new {
-    val `scala2.12`      = "2.12.4"
+    val `scala2.12`      = "2.12.5"
     val `scala2.11`      = "2.11.12"
     val scalafmt         = "1.3.0"
     val cats             = "1.1.0"
