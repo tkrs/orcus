742baa0ece4b64342998c7d2affd89e1de38a182
Takeru Sato <type.in.type@gmail.com>
2020-03-02 16:21:56 +0900

    feat: omit the unnecessary ExecutionContext

--- modules/cats-effect/src/main/scala/orcus/async/instances/catsEffect/AsyncHandlerConcurrentEffectInstances.scala
+++ modules/cats-effect/src/main/scala/orcus/async/instances/catsEffect/AsyncHandlerConcurrentEffectInstances.scala
@@ -3,14 +3,12 @@ package orcus.async.instances.catsEffect
 import cats.effect.ConcurrentEffect
 import orcus.async.AsyncHandler
 
-import scala.concurrent.ExecutionContext
-
 private[catsEffect] trait AsyncHandlerConcurrentEffectInstances {
-  implicit def handleConcurrentEffect[F[_]](implicit F: ConcurrentEffect[F], ec: ExecutionContext): AsyncHandler[F] =
+  implicit def handleConcurrentEffect[F[_]](implicit F: ConcurrentEffect[F]): AsyncHandler[F] =
     new AsyncHandler[F] {
       def handle[A](callback: AsyncHandler.Callback[A], cancel: => Unit): F[A] =
         F.cancelable[A] { cb =>
-          ec.execute(() => callback(cb))
+          callback(cb)
           F.delay(cancel)
         }
     }
