a267994d513e3aa9bf79bad4c98345278da56b4c
Scala Steward <me@scala-steward.org>
2020-10-27 23:22:26 +0100

    Update util-core to 20.10.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
 
     val cats        = "2.2.0"
     val monix       = "3.2.2"
-    val twitterUtil = "20.9.0"
+    val twitterUtil = "20.10.0"
     val catsEffect  = "2.2.0"
     val shapeless   = "2.3.3"
     val scalatest   = "3.2.2"
