bc9d5b8bb58e3fb6894ead0c70c844cda23f0325
Scala Steward <me@scala-steward.org>
2020-12-19 06:52:59 +0100

    build(deps): bump util-core from 20.10.0 to 20.12.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
 
     val cats        = "2.3.1"
     val monix       = "3.3.0"
-    val twitterUtil = "20.10.0"
+    val twitterUtil = "20.12.0"
     val catsEffect  = "2.3.1"
     val shapeless   = "2.3.3"
     val scalatest   = "3.2.3"
