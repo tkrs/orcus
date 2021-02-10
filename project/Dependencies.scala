aaff50e76fb391189900c928dec3a47e1d9d344b
Scala Steward <me@scala-steward.org>
2021-02-10 19:15:50 +0100

    build(deps): bump cats-core, cats-free from 2.3.1 to 2.4.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,10 +8,10 @@ object Dependencies {
     val kindProjector   = "0.11.3"
     val organizeImports = "0.5.0"
 
-    val cats        = "2.3.1"
+    val cats        = "2.4.1"
     val monix       = "3.3.0"
     val twitterUtil = "20.10.0"
-    val catsEffect  = "2.3.1"
+    val catsEffect  = "2.4.1"
     val shapeless   = "2.3.3"
     val scalatest   = "3.2.3"
     val scalatestplus = new {
