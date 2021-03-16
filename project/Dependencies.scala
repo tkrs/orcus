630f75c38707bb59030b27bbbc2a8d9936b6d046
Scala Steward <me@scala-steward.org>
2021-03-16 18:57:03 +0100

    build(deps): bump google-cloud-bigtable from 1.21.0 to 1.21.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies {
     }
 
     val hbase            = "2.4.1"
-    val bigtable         = "1.21.0"
+    val bigtable         = "1.21.1"
     val logback          = "1.2.3"
     val scalaLogging     = "3.9.2"
     val scalaJava8Compat = "0.9.1"
