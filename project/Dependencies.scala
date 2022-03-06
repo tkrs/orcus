8942912e40e8541a0b4b94f6b0c31a54233deee9
Scala Steward <me@scala-steward.org>
2022-03-06 01:08:00 +0100

    build(deps): bump logback-classic from 1.2.6 to 1.2.11

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -21,7 +21,7 @@ object Dependencies {
 
     val hbase            = "2.4.6"
     val bigtable         = "2.1.4"
-    val logback          = "1.2.6"
+    val logback          = "1.2.11"
     val scalaLogging     = "3.9.4"
     val scalaJava8Compat = "1.0.0"
   }
