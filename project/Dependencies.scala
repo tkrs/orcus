98feabc926d056c79d8f5094d739d23da83f2393
Scala Steward <me@scala-steward.org>
2022-04-06 02:37:36 +0200

    build(deps): bump cats-effect from 2.5.4 to 3.3.11

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
     val cats        = "2.7.0"
     val monix       = "3.4.0"
     val twitterUtil = "21.8.0"
-    val catsEffect  = "2.5.4"
+    val catsEffect  = "3.3.11"
     val shapeless   = "2.3.8"
     val scalatest   = "3.2.11"
     val scalatestplus = new {
