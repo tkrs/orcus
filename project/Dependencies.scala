93ca897ef19d1f15802b14f10aee1deae0e6f883
Scala Steward <scala_steward@virtuslab.com>
2022-08-30 17:14:39 +0000

    build(deps): bump hbase-client from 2.4.13 to 2.4.14

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Dependencies {
       val mockito    = "3.2.10.0"
     }
 
-    val hbase            = "2.4.13"
+    val hbase            = "2.4.14"
     val bigtable         = "2.9.0"
     val logback          = "1.2.11"
     val scalaLogging     = "3.9.5"
