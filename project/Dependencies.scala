bd611662c789012c5176615974d79b62b6059686
Scala Steward <scala_steward@virtuslab.com>
2023-02-14 20:16:03 +0000

    build(deps): bump cats-effect from 3.4.4 to 3.4.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
     val organizeImports = "0.6.0"
 
     val cats       = "2.9.0"
-    val catsEffect = "3.4.4"
+    val catsEffect = "3.4.7"
     val shapeless  = "2.3.10"
     val shapeless3 = "3.3.0"
     val scalatest  = "3.2.14"
