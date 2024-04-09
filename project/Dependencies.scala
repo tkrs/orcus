6c32131492fdac340f606240b41ea478e5fde7db
Scala Steward <scala_steward@virtuslab.com>
2024-04-09 19:33:32 +0000

    build(deps): bump logback-classic from 1.5.3 to 1.5.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies {
     }
 
     val bigtable         = "2.36.0"
-    val logback          = "1.5.3"
+    val logback          = "1.5.4"
     val scalaLogging     = "3.9.5"
     val scalaJava8Compat = "1.0.2"
   }
