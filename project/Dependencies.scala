5e3e2a8bc941ca18e760e5e3069d9e669f413caa
Scala Steward <scala_steward@virtuslab.com>
2024-05-17 19:31:11 +0000

    build(deps): bump shapeless from 2.3.10 to 2.3.11

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
 
     val cats       = "2.10.0"
     val catsEffect = "3.5.4"
-    val shapeless  = "2.3.10"
+    val shapeless  = "2.3.11"
     val shapeless3 = "3.4.1"
     val scalatest  = "3.2.18"
     val scalatestplus = new {
