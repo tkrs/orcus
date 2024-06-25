0dfa3661fda0f7730b5318def14173c45781929f
Scala Steward <scala_steward@virtuslab.com>
2024-06-25 04:44:39 +0000

    build(deps): bump scalatest from 3.2.18 to 3.2.19

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
     val catsEffect = "3.5.4"
     val shapeless  = "2.3.12"
     val shapeless3 = "3.4.1"
-    val scalatest  = "3.2.18"
+    val scalatest  = "3.2.19"
     val scalatestplus = new {
       val scalacheck = "3.2.17.0"
       val mockito    = "3.2.15.0"
