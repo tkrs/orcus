0435b8d3dfee68081fef4d6f9510bd0b64c77b4e
Scala Steward <scala_steward@virtuslab.com>
2025-01-02 19:51:02 +0000

    build(deps): bump google-cloud-bigtable from 2.48.0 to 2.51.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
       val mockito    = "3.2.15.0"
     }
 
-    val bigtable         = "2.48.0"
+    val bigtable         = "2.51.0"
     val logback          = "1.5.15"
     val scalaLogging     = "3.9.5"
     val scalaJava8Compat = "1.0.2"
