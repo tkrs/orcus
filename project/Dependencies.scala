382668266c618eb0e8c3eae5b5aba322974868f9
Scala Steward <scala_steward@virtuslab.com>
2025-01-22 22:07:52 +0000

    build(deps): bump scala3-library from 3.6.2 to 3.6.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   val V = new {
     val `scala2.13` = "2.13.16"
-    val scala3      = "3.6.2"
+    val scala3      = "3.6.3"
 
     val kindProjector = "0.13.2"
 
